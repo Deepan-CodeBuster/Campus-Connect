21d8acb62e6d92c0199add56848d1285b58bac6d
Deepan-CodeBuster <112kdeepan@gmail.com>
2025-09-17 13:37:19 +0530

    updated app.py

--- app.py
+++ app.py
@@ -293,5 +293,8 @@ def comment_post(post_id):
     return redirect(url_for("view_posts"))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
